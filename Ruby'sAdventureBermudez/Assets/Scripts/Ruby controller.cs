4ef8341fdececb62a2b414fdd3268a4369513a58
Daniel Bermudez <741757@student.dusd.net>
2023-11-14 14:45:20 -0800

    Repaired Ridgedbody component
    Upgraded ruby's movement code to remove jittering and unwanted rotation

--- Ruby'sAdventureBermudez/Assets/Scripts/Ruby controller.cs
+++ Ruby'sAdventureBermudez/Assets/Scripts/Ruby controller.cs
@@ -4,22 +4,27 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
     void Start()
     {
-        //QualitySettings.vSyncCount = 0;
-        //Application.targetFrameRate = 10;
+        rigidbody2d = GetComponent<Rigidbody2D>(); 
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         float horizontal = Input.GetAxis("Horizontal");
         float Vertical = Input.GetAxis("Vertical");
-       
+         
         Vector2 position = transform.position;
-        position.x = position.x + 3f * horizontal * Time.deltaTime;
-        position.y = position.y + 3f * Vertical * Time.deltaTime;
-       transform.position = position;
+        position.x = position.x + 10f * horizontal * Time.deltaTime;
+        position.y = position.y + 10f * Vertical * Time.deltaTime;
+       
+
+        rigidbody2d.MovePosition(position);
     }
 }
