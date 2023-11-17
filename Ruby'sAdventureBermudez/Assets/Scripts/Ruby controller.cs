f75f8edbfa33e12fac125c090850bc016c05d996
Daniel Bermudez <144481244+Bermudez-Daniel@users.noreply.github.com>
2023-11-17 11:38:21 -0800

    I added A health variable and a new code for the berry
    I added new items and sadly had to remove Broly and Goku

--- Ruby'sAdventureBermudez/Assets/Scripts/Ruby controller.cs
+++ Ruby'sAdventureBermudez/Assets/Scripts/Ruby controller.cs
@@ -1,30 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    public float speed = 3f;
+    
+    public int maxHealth = 5;
+    
+    
+    public int health { get { return maxHealth; } } 
+    int currentHealth;
+
+
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
+
+
     void Start()
     {
-        rigidbody2d = GetComponent<Rigidbody2D>(); 
-        
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
+        currentHealth = 1;
     }
-
     // Update is called once per frame
     void Update()
     {
-        
-        float horizontal = Input.GetAxis("Horizontal");
-        float Vertical = Input.GetAxis("Vertical");
-         
-        Vector2 position = transform.position;
-        position.x = position.x + 10f * horizontal * Time.deltaTime;
-        position.y = position.y + 10f * Vertical * Time.deltaTime;
-       
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
     }
+
+    public void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+    }
+
 }
