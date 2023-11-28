8d1023a7224911a19c46faedf1f062d22123998c
Daniel Bermudez <144481244+Bermudez-Daniel@users.noreply.github.com>
2023-11-28 10:25:34 -0800

    merging repo

--- Ruby'sAdventureBermudez/Assets/Scripts/Ruby controller.cs
+++ Ruby'sAdventureBermudez/Assets/Scripts/Ruby controller.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+<<<<<<< HEAD
     public float speed = 3f;
     
     public int maxHealth = 5;
@@ -10,23 +11,77 @@ public class RubyController : MonoBehaviour
     public int health { get { return maxHealth; } } 
     int currentHealth;
 
+=======
+    public float speed = 3.0f;
+
+    public int maxHealth = 5;
+
+    public GameObject projectilePrefab;
+
+    public int health { get { return currentHealth; } }
+    int currentHealth;
+
+    public float timeInvincible = 2.0f;
+    bool isInvincible;
+    float invincibleTimer;
+>>>>>>> 92942875ff76fa4aee630faab3c14e724c557825
 
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
 
+<<<<<<< HEAD
 
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
         currentHealth = 1;
+=======
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+
+        currentHealth = maxHealth;
+>>>>>>> 92942875ff76fa4aee630faab3c14e724c557825
     }
     // Update is called once per frame
     void Update()
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+<<<<<<< HEAD
+=======
+
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+                isInvincible = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Launch();
+        }
+>>>>>>> 92942875ff76fa4aee630faab3c14e724c557825
     }
 
     void FixedUpdate()
@@ -40,8 +95,32 @@ public class RubyController : MonoBehaviour
 
     public void ChangeHealth(int amount)
     {
+<<<<<<< HEAD
+=======
+        if (amount < 0)
+        {
+            if (isInvincible)
+                return;
+
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
+
+>>>>>>> 92942875ff76fa4aee630faab3c14e724c557825
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
 
+<<<<<<< HEAD
+=======
+    void Launch()
+    {
+        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch(lookDirection, 300);
+
+        animator.SetTrigger("Launch");
+    }
+>>>>>>> 92942875ff76fa4aee630faab3c14e724c557825
 }
